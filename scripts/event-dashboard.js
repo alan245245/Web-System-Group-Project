f2ccf96873c2cc59f3ea87b81e7a3029ec4e2860
Andy891 <andyfoo891@gmail.com>
2023-10-25 18:21:31 +0800

    update event-dashboard

--- scripts/event-dashboard.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    if(window.innerWidth > 1000){
-        $("#small-table").addClass("d-none");
-    } else{
-        $("#large-table").addClass("d-none");
-    }
-});
