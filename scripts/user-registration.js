0e455de86991407d720893a7b04c09a9a6465025
alan245245 <alanalanyeung@gmail.com>
2023-11-03 14:57:25 +0800

    Finish password strength meter

--- scripts/user-registration.js
+++ scripts/user-registration.js
@@ -1,6 +1,9 @@
 $.when($(document).ready).then(function() {
-
+    $("#user-password").on("input", function() {
+        updateMeter();
+    })
 })
+
 
 function validateForm() {
     let firstName;
@@ -149,4 +152,67 @@ function validateForm() {
     }
 
     return valid;
+}
+
+function updateMeter() {
+    const ele = $("#user-password");
+    const meterSections = document.querySelectorAll('.meter-section');
+    password = ele.val();
+    
+    const strength = calculatePasswordStrength(password);
+
+    // Remove all strength classes
+    meterSections.forEach((section) => {
+        section.classList.remove('weak', 'medium', 'strong', 'very-strong');
+    });
+
+    // Add the appropriate strength class based on the strength value
+    if (strength >= 1) {
+        meterSections[0].classList.add('weak');
+    }
+    if (strength >= 2) {
+        meterSections[1].classList.add('medium');
+    }
+    if (strength >= 3) {
+        meterSections[2].classList.add('strong');
+    }
+    if (strength >= 4) {
+        meterSections[3].classList.add('very-strong');
+    }
+}
+
+function calculatePasswordStrength(pw) {
+    const password = pw;
+    const lengthWeight = 0.2;
+    const uppercaseWeight = 0.5;
+    const lowercaseWeight = 0.5;
+    const numberWeight = 0.7;
+    const symbolWeight = 1;
+
+    let strength = 0;
+
+    // Password strength based on length
+    strength += password.length * lengthWeight;
+
+    // Password strength based on uppercase letters
+    if (/[A-Z]/.test(password)) {
+        strength += uppercaseWeight;
+    }
+    
+    // Password strength based on lowercase letters
+    if (/[a-z]/.test(password)) {
+        strength += lowercaseWeight;
+    }
+
+    // Password strength based on numbers
+    if (/\d/.test(password)) {
+        strength += numberWeight;
+    }
+
+    // Password strength based on symbols
+    if (/[^A-Za-z0-9]/.test(password)) {
+        strength += symbolWeight;
+    }
+
+    return strength;
 }
