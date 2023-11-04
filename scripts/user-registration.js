f239c0c40ef01268616f76b4811976bda60fb169
alan245245 <alanalanyeung@gmail.com>
2023-11-05 00:59:26 +0800

    Code cleanup and commenting

--- scripts/user-registration.js
+++ scripts/user-registration.js
@@ -1,19 +1,28 @@
 $.when($(document).ready).then(function() {
+    // When the user input the password, update the strength meter
     $("#user-password").on("input", function() {
         updateMeter();
     });
 
+    // Show the password suggestions when focused
     $("#user-password").on("focus", function() {
         $("#password-suggestion").addClass("d-flex").removeClass("d-none");
     });
-
+    
+    // Hide the password suggestions when not focused
     $("#user-password").on("blur", function() {
         $("#password-suggestion").addClass("d-none").removeClass("d-flex");
     });
 })
 
-
-function validateForm() {
+/**
+ * Validate the input field of user registration
+ *
+ * @param event of the form submit
+ * @returns Bolleans - True when the form has no error detected
+ */
+function validateForm(event) {
+    event.preventDefault();
     let firstName;
     let lastName;
     let gender;
@@ -23,100 +32,128 @@ function validateForm() {
     let email;
     let valid = true;
 
+    /* 
+    Input checking for first name 
+    - Check for no input
+    */
     try {
         const ele = $("#user-firstname");
         firstName = ele.val();
         firstName = firstName.trim();
         if (firstName == '') {
             // Invalid: Missing input
-            ele.removeClass("is-valid");
-            ele.addClass("is-invalid");
+            ele.removeClass("is-valid").addClass("is-invalid");
             valid = false;
         } else {
-            ele.removeClass("is-invalid");
-            ele.addClass("is-valid");
+            // Valid
+            ele.removeClass("is-invalid").addClass("is-valid");
         }
     } catch (error) {
-        alert(error.message);
+        // Invalid: Fetch error
+        const ele = $("#user-firstname");
+        ele.removeClass("is-valid").addClass("is-invalid");
+        valid = false;
     }
 
+    /* 
+    Input checking for last name 
+    - Check for no input
+    */
     try {
         const ele = $("#user-lastname");
         lastName = ele.val();
         lastName = lastName.trim();
         if (lastName == '') {
             // Invalid: Missing input
-            ele.removeClass("is-valid");
-            ele.addClass("is-invalid");
+            ele.removeClass("is-valid").addClass("is-invalid");
             valid = false;
         } else {
-            ele.removeClass("is-invalid");
-            ele.addClass("is-valid");
+            // Valid
+            ele.removeClass("is-invalid").addClass("is-valid");
         }
     } catch (error) {
-        alert(error.message);
+        // Invalid: Fetch error
+        const ele = $("#user-lastname");
+        ele.removeClass("is-valid").addClass("is-invalid");
+        valid = false;
     }
 
+    /* 
+    Input checking for gender 
+    - Check for no input
+    */
     try {
         gender = document.querySelector('input[name="gender"]:checked').value;
         const ele = $("#female");
         if (gender == '') {
             // Invalid: Missing input
-            ele.removeClass("is-valid");
-            ele.addClass("is-invalid");
+            ele.removeClass("is-valid").addClass("is-invalid");
             valid = false;
         } else {
-            ele.removeClass("is-invalid");
-            ele.addClass("is-valid");
+            // Valid
+            ele.removeClass("is-invalid").addClass("is-valid");
         }
     } catch (error) {
+        // Invalid: Fetch error
         const ele = $("#female");
-        ele.removeClass("is-valid");
-        ele.addClass("is-invalid");
+        ele.removeClass("is-valid").addClass("is-invalid");
         valid = false;
     }
 
+    
+    /* 
+    Input checking for birthday 
+    - Check for no input
+    */
     try {
         const ele = $("#user-birthday");
         birthday = ele.val();
         birthday = birthday;
         if (birthday == '') {
             // Invalid: Missing input
-            ele.removeClass("is-valid");
-            ele.addClass("is-invalid");
+            ele.removeClass("is-valid").addClass("is-invalid");
             valid = false;
         } else {
-            ele.removeClass("is-invalid");
-            ele.addClass("is-valid");
+            // Valid
+            ele.removeClass("is-invalid").addClass("is-valid");
         }
     } catch (error) {
+        // Invalid: Fetch error
         const ele = $("#user-birthday");
-        ele.removeClass("is-valid");
-        ele.addClass("is-invalid");
+        ele.removeClass("is-valid").addClass("is-invalid");
         valid = false;
     }
 
-    
+    /* 
+    Input checking username 
+    - Check for no input
+    - Check for duplicates
+    */
     try {
         const ele = $("#user-name");
         username = ele.val();
         username = username;
         if (username == '') {
             // Invalid: Missing input
-            ele.removeClass("is-valid");
-            ele.addClass("is-invalid");
+            ele.removeClass("is-valid").addClass("is-invalid");
             valid = false;
+        } else if (false) {
+            // Invalid: Duplicated username
         } else {
-            ele.removeClass("is-invalid");
-            ele.addClass("is-valid");
+            ele.removeClass("is-invalid").addClass("is-valid");
         }
     } catch (error) {
+        // Invalid: Fetch error
         const ele = $("#user-name");
-        ele.removeClass("is-valid");
-        ele.addClass("is-invalid");
+        ele.removeClass("is-valid").addClass("is-invalid");
         valid = false;
     }
 
+    /* 
+    Input checking for password 
+    - Check for no input
+    - Check for password strength
+    */
     try {
         const ele = $("#user-password");
         const msg = $("#invalid-feedback-password");
@@ -125,47 +162,59 @@ function validateForm() {
         const strength = calculatePasswordStrength(password);
         if (password == '') {
             // Invalid: Missing input
-            ele.removeClass("is-valid");
-            ele.addClass("is-invalid");
+            ele.removeClass("is-valid").addClass("is-invalid");
             valid = false;
         } else if (strength <= 2) {
-            ele.removeClass("is-valid");
-            ele.addClass("is-invalid");
+            // Invalid: Password not strong enough
+            ele.removeClass("is-valid").addClass("is-invalid");
         } else {
-            ele.removeClass("is-invalid");
-            ele.addClass("is-valid");
+            // Valid
+            ele.removeClass("is-invalid").addClass("is-valid");
         }
     } catch (error) {
+        // Invalid: Fetch error
         const ele = $("#user-password");
-        ele.removeClass("is-valid");
-        ele.addClass("is-invalid");
+        ele.removeClass("is-valid").addClass("is-invalid");
         valid = false;
     }
 
+    /* 
+    Input checking for email 
+    - Check for no input
+    - Check for email patterns
+    */
     try {
         const ele = $("#user-email");
         email = ele.val();
         email = email.toLowerCase().trim();
         if (email == '') {
             // Invalid: Missing input
-            ele.removeClass("is-valid");
-            ele.addClass("is-invalid");
+            ele.removeClass("is-valid").addClass("is-invalid");
             valid = false;
         } else if (!email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
-            // Invalid: Invalid email
-            ele.removeClass("is-valid");
-            ele.addClass("is-invalid");
+            // Invalid: Invalid email pattern
+            ele.removeClass("is-valid").addClass("is-invalid");
         } else {
-            ele.removeClass("is-invalid");
-            ele.addClass("is-valid");
+            // Valid
+            ele.removeClass("is-invalid").addClass("is-valid");
         }
     } catch (error) {
-        alert(error.message);
+        // Invalid: Fetch error
+        const ele = $("#user-email");
+        ele.removeClass("is-valid").addClass("is-invalid");
+        valid = false;
     }
 
+    // Function return valid if no error is detected
     return valid;
 }
 
+/**
+ * Update password strength meter
+ *
+ *
+ *
+ */
 function updateMeter() {
     const ele = $("#user-password");
     const meterSections = document.querySelectorAll('.meter-section');
@@ -193,6 +242,12 @@ function updateMeter() {
     }
 }
 
+/**
+ * Calcalate password strength
+ *
+ * @param password
+ * @returns float
+ */
 function calculatePasswordStrength(pw) {
     const password = pw;
     const lengthWeight = 0.2;
