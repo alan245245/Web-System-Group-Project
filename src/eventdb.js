370be488ce7e7c929845d730a9fa48c3ebb591c3
alan245245 <alanalanyeung@gmail.com>
2023-11-21 00:45:34 +0800

    Sample events and event db API

---
+++ src/eventdb.js
@@ -0,0 +1,38 @@
+import fs from "fs/promises";
+import client from "./dbclient.js";
+import crypto from "crypto";
+
+async function init_db() {
+    try {
+        // TODO
+        const events = client.db("projectdb").collection("event");
+
+        if ((await events.countDocuments()) == 0) {
+            const jsonString = await fs.readFile("./src/events.json");
+            const jsonObject = await JSON.parse(jsonString);
+
+            const insertCount = (await events.insertMany(jsonObject)).insertedCount;
+            console.log(`Added ${insertCount} events`);
+        }
+    } catch (err) {
+        // TODO
+        console.log(`Unable to intialize the database!` + err);
+    }
+}
+
+async function getAllEvents() {
+    try {
+        const events = client.db("projectdb").collection("event");
+
+        if ((await events.countDocuments()) == 0) init_db();
+
+        let result = await events.find().toArray();
+        return result;
+    } catch (err) {
+        console.log(`ERROR: Unable to get events.`);
+    }
+}
+
+async function createEvents(title, origin, destination, priceEconomic, priceFirst, trainNumber, eventTime) {}
+
+export default { getAllEvents };
