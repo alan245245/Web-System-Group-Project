73131dfb78229c6cdf7fb9bf936d81b0d4f745cd
Andy891 <andyfoo891@gmail.com>
2023-11-28 21:11:17 +0800

    adjust the color of the list

--- static/scripts/event-dashboard.js
+++ static/scripts/event-dashboard.js
@@ -11,27 +11,29 @@ $().ready(() => {
                 let at = new Date(parseInt(jsonObject.events[i].arriveTime));
                 $("#event-container").append(`
                     <ul class="list-group list-group-horizontal-xl text-center">
-                        <li class="list-group-item flex-fill fw-bold">${jsonObject.events[i].trainNumber}</li>
-                        <li class="list-group-item flex-fill">
+                        <li class="list-group-item ${
+                            i % 2 == 1 ? "list-group-item-secondary" : ""
+                        } flex-fill fw-bold">${jsonObject.events[i].trainNumber}</li>
+                        <li class="list-group-item ${i % 2 == 1 ? "list-group-item-secondary" : ""} flex-fill">
                             <div class="text-primary">${dt.getHours() < 10 ? "0" : ""}${dt.getHours()}:${
                                 dt.getMinutes() < 10 ? "0" : ""
                             }${dt.getMinutes()}</div>
                             ${jsonObject.events[i].origin}
                         </li>
-                        <li class="list-group-item flex-fill">
+                        <li class="list-group-item ${i % 2 == 1 ? "list-group-item-secondary" : ""} flex-fill">
                             ${at.getHours() - dt.getHours() < 10 ? "0" : ""}${at.getHours() - dt.getHours()}:${
                                 at.getMinutes() - dt.getMinutes() < 10 ? "0" : ""
                             }${at.getMinutes() - dt.getMinutes()}<br />
                             <img class="d-none d-xl-inline" src="assets/arrow.png" width="60px" />
                             <img class="d-inline d-xl-none" src="assets/vertical-arrow.png" width="30px" />
                         </li>
-                        <li class="list-group-item flex-fill">
+                        <li class="list-group-item ${i % 2 == 1 ? "list-group-item-secondary" : ""} flex-fill">
                             <div class="text-primary">${at.getHours() < 10 ? "0" : ""}${at.getHours()}:${
                                 at.getMinutes() < 10 ? "0" : ""
                             }${at.getMinutes()}</div>
                             ${jsonObject.events[i].destination}
                         </li>
-                        <li class="list-group-item flex-fill">
+                        <li class="list-group-item ${i % 2 == 1 ? "list-group-item-secondary" : ""} flex-fill">
                             First Class: $${jsonObject.events[i].priceFirst}<br class="d-inline d-xl-none" />
                             <span class="badge text-bg-secondary">Available seat: ${
                                 jsonObject.events[i].occupiedSeats
